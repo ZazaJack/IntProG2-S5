d87d865d577be16eb7e8e9b2883214ea95887d24
ZazaJack <rzahidbenjamin1@gmail.com>
2025-04-22 10:41:49 -0600

    Le hice una correción a las variables

--- Actividad_3/Ejercicio4.py
+++ Actividad_3/Ejercicio4.py
@@ -4,13 +4,13 @@ litros_precio = 49.57 # Precio del litro de gasolina
 distancia_km = float(input("Ingrese la distancia recorrida en kilómetros: "))
 litros_consumidos = float(input("Ingrese la cantidad de litros consumidos en el trayecto: "))
 
-división = distancia_km / litros_consumidos
+division = distancia_km / litros_consumidos
 
 
-multiplicación = litros_precio * litros_consumidos
+multiplicacion = litros_precio * litros_consumidos
 
 print(f"La distancia recorrida es de {distancia_km} km")
 print(f"Litros de gasolina consumidos: {litros_consumidos} litros")
 print(f"El precio del litro de gasolina es de {litros_precio} pesos")
-print(f"El rendimiento del vehículo es de {división:.2f} km/litro")
-print(f"El gasto total de gasolina es de {multiplicación:.2f} pesos")
+print(f"El rendimiento del vehículo es de {division:.2f} km/litro")
+print(f"El gasto total de gasolina es de {multiplicacion:.2f} pesos")
