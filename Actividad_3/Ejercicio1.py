05c62316c465e462e520111722c058b3f0893a92
ZazaJack <rzahidbenjamin1@gmail.com>
2025-04-22 07:51:41 -0600

    Se realizo un formato a la salida

--- Actividad_3/Ejercicio1.py
+++ Actividad_3/Ejercicio1.py
@@ -6,7 +6,7 @@ calificacion3 = int(input("Ingrese la calificación del estudiante 3: "))
 suma = calificacion1 + calificacion2 + calificacion3
 promedio = suma / 3
 
-print(f"""Calfificaión 1: {calificacion1}
-Calificación 2: {calificacion2}
-Calificacion 3: {calificacion3}      
-promedio: {promedio: .0f}""")
+print(f"""Calfificaión 1: {calificacion1:>3}
+Calificación 2: {calificacion2:>3}
+Calificacion 3: {calificacion3:>3}      
+{"Promedio:":<15} {promedio:>3.0f}""")
