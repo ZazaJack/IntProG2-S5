4850a7be522ad109ece2b29faa80b48447465dab
ZazaJack <rzahidbenjamin1@gmail.com>
2025-04-25 07:45:17 -0600

    Programa para aplicar el bono a un empleado

---
+++ Actividad_5/bono_empleado.py
@@ -0,0 +1,12 @@
+bono = 0
+sueldo_empleado = float(input("Ingrese el sueldo del empleado: "))
+if sueldo_empleado < 3000:
+    bono = sueldo_empleado * 0.10
+elif sueldo_empleado >= 1500 and sueldo_empleado <= 3000:
+    bono = sueldo_empleado * 0.05
+elif sueldo_empleado <= 1500:
+    print("El empleado no tiene derecho a bono.")
+else:
+    print("El sueldo ingresado no es válido. Debe ser un número positivo.")
+print("El bono del empleado es: ", bono)
+
